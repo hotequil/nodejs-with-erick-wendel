79d108660813b45dfe1c442fe15b782fb8706d07
hotequil <hotequil@protonmail.com>
2021-12-18 22:14:38 -0300

    done create test.

--- nineth/src/tests/mongo.test.js
+++ nineth/src/tests/mongo.test.js
@@ -1,11 +1,18 @@
-const { ok } = require('assert');
+const { ok, deepEqual } = require('assert');
 const Context = require('../db/base/context');
 const Mongo = require('../db/mongo');
 const context = new Context(new Mongo());
+const LANGUAGE_TO_CREATE = { name: 'C#', extension: '.cs' };
 
 describe('Mongo', function(){
     this.timeout(Infinity);
     this.beforeAll(async () => await context.connect());
 
     it('Should is connected when was called', async () => ok(await context.isConnected()));
+
+    it('Should create a new language when was called', async () => {
+        const { name, extension } = await context.create(LANGUAGE_TO_CREATE);
+
+        deepEqual({ name, extension }, LANGUAGE_TO_CREATE);
+    });
 });
